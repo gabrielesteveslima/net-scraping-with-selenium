3ef295e5678f288fc252cfbfbfd4b8e755fced14
Gabriel Esteves Lima <gabriel.esteves@git.com.br>
2020-10-29 10:08:25 -0300

    adjusting powershell

--- run-infrastructure.ps1
+++ run-infrastructure.ps1
@@ -1,5 +1,5 @@
 Set-Location -Path "src/service/sib-sample-infrastructure"
 
 dotnet tool restore;
-dotnet ef migrations add UpInitalSchemaTables -o \Design\Migrations;
+dotnet ef migrations add UpInitalSchemaTables -o Database\Design\Migrations;
 dotnet ef database update;
